db70781f1bd07b552289f8cf64463e19b49e5b6a
khoi tran <khoitran@misfit.com>
2016-08-10 05:59:14 +0700

    Checking session in Admin pages.

--- WebApplication1/Controllers/Admin/AdminController.cs
+++ WebApplication1/Controllers/Admin/AdminController.cs
@@ -13,5 +13,10 @@ namespace WebApplication1.Controllers.Admin
         {
             return View();
         }
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
--- WebApplication1/Controllers/Admin/BaseAdminController.cs
+++ WebApplication1/Controllers/Admin/BaseAdminController.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
+    [ActionExcuted]
     public abstract class BaseAdminController : Controller
     {
         public DataClassesDataContext data = new DataClassesDataContext();
-      
-        public BaseAdminController()
+    }
+
+    public class ActionExcutedAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
-            //TODO, do anything we need all controllers to inherit.
+            base.OnActionExecuted(filterContext);
+            bool isCurrentLyInLoginPage = filterContext.ActionDescriptor.ActionName.Equals("Login");
+            if (!hasAdminLoginSession() && !isCurrentLyInLoginPage)
+            {
+                //Redirect to page Login Admin
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                {
+                    controller = "Admin",
+                    action = "Login"
+                }));
+            }
+        }
+
+        private bool hasAdminLoginSession()
+        {
+            return true;
         }
     }
 }
